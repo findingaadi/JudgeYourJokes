81ccf8b11f48d3b849c438a95d6224007d37f959
findingaadi <adarshaaryal30@gmail.com>
2025-09-18 21:59:00 +0100

    jokes page controller added

--- Controllers/JokesController.cs
+++ Controllers/JokesController.cs
@@ -3,17 +3,20 @@ using Microsoft.AspNetCore.Mvc;
 using JudgeYourJokes.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
 
 namespace JudgeYourJokes.Controllers
 {
     public class JokesController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
 
-
-        public JokesController(ApplicationDbContext context)
+        public JokesController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
 
@@ -21,6 +24,28 @@ namespace JudgeYourJokes.Controllers
         {
             var jokes = _context.Jokes.ToList();
             return View(jokes);
+        }
+
+        //Get: Jokes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        //Post: Jokes/Create
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Jokes joke)
+        {
+           joke.UserID = _userManager.GetUserId(User);  
+           joke.Date = DateTime.Now;
+           joke.Votes = 0;
+
+           _context.Jokes.Add(joke);
+           await _context.SaveChangesAsync();
+           return RedirectToAction(nameof(Index));
         }
     }
 }
